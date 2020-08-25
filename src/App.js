4275e4f40ba7ccf3ac0268c213bc2dbe213c57bc
Madeline Dutro <dutro.madeline@gmail.com>
2020-08-24 22:24:38 -0400

    fleshing out display component, prepping for AJAX call

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import heroDog from './dogfrisbee2.jpg';
+import dogDiv from './dogdisplay.js';
+
+function getDog() {
+  //fetch from dogAPI
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mainDiv">
+      <div className="heroDiv">
+        <img src={heroDog} alt="dog jumping and catching frisbee"></img>
+        <h1>Dogspotting the Best</h1>
+      </div>
+      <div id="actionDiv">
+        <button>Sign Up</button>
+        <button>Get a New Dog</button>
+      </div>
+      <div id="faqDiv">
+        {/* This is where FAQ's go */}
+      </div>
+      {dogDiv}
     </div>
   );
 }
 
 export default App;
+
+
+/* const data = { username: 'example' };
+
+fetch('https://example.com/profile', {
+  method: 'POST', // or 'PUT'
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(data),
+})
+.then(response => response.json())
+.then(data => {
+  console.log('Success:', data);
+})
+.catch((error) => {
+  console.error('Error:', error);
+}); */
+
